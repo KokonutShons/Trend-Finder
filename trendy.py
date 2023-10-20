5292ebc1f2291fa17d9feb8bbe2f2e2a77a6b370
KokonutShons <seanfujiwara2001@gmail.com>
2023-10-19 23:13:40 -0700

    V2

--- trendy.py
+++ trendy.py
@@ -13,24 +13,27 @@ trending_container = soup.find('div', class_='feed-list-wrapper')
 
 if trending_container :
     # Find all trending topics within the container
-    first_topic = trending_container.find_all('div', class_='md-list-block first-list-item')
-    trending_topics = trending_container.find_all('div', class_='md-list-block')
+    # first_topic = trending_container.find_all('div', class_='md-list-block first-list-item')
+    # trending_topics = trending_container.find_all('div', class_='md-list-block')
+    trending_topics = trending_container.find_all('div', class_='trending-item')
 
     # do just first
-    beets = first_topic.find_all('div', class_='details-top')
+    beets = trending_topics[0].find('div', class_='details-top')
     tupac = beets.find('a').text
-    print(tupac)
+    print(f"1, {tupac}")
 
     # Extract the second and third trending topics
-    sndtrd = []
-    for topic in trending_topics[1:3] :
-        deets = topic.find_all('div', class_='details-top')
-        # Navigate through the nested elements to get the topic title
-        topic_title = deets.find('a').text
-        sndtrd.append(topic_title)
+    # sndtrd = []
+    # for topic in trending_topics[1:3] :
+    #     deets = topic.find_all('div', class_='details-top')
+    #     # Navigate through the nested elements to get the topic title
+    #     topic_title = deets.find('a').text
+    #     sndtrd.append(topic_title)
 
-    for i, topic in enumerate(sndtrd, start=1) :
-        print(f"{i}, {topic}")
+    for i, topic in enumerate(trending_topics[1:3], start=2) :
+        deets = topic.find('div', class_='details-top')
+        topi_titl = deets.find('a').text
+        print(f"{i}, {topi_titl}")
 else :
     print("trending topics container not found on the page.")
 
